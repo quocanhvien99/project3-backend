abdc25c87ebc314c733e8aefe048e6c7d084cb35
Vien Quoc Anh <quocanhvien99@gmail.com>
2022-01-25 17:31:15 +0700

    .

--- cron.js
+++ cron.js
@@ -3,8 +3,8 @@ const pgPool = require('./Utils/pgPool');
 const cron = require('node-cron');
 
 module.exports = () => {
-	//check at 6pm
-	cron.schedule('0 5 * * *', () => {
+	//check at 5:30pm
+	cron.schedule('30 17 * * *', () => {
 		let now = new Date(Date.now());
 		const query = `select U.email, T.end_time, T.heading from "task" as T, "user" as U where T.user_id=U.id and completed = $2 and end_time >= ($1::date) and end_time <= ($1::date + '5 days'::interval)`;
 		pgPool
